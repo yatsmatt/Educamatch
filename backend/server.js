3b106e9bcd2c9bd1713c6a66e9a21499dbc53d83
yatsilmatt <yatsilmatt@gmail.com>
2023-08-06 13:28:52 +0300

    adding routes and controller strucutre

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,16 @@
-console.log("hello");
+const e = require("express");
+const express = require("express");
+const donenv = require("dotenv").config();
+const { errorHeanler } = require("./middleware/errormiddleware");
+const port = process.env.port || 5000;
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+//route to routes
+app.use("/api/goals", require("./routes/goalRoutes"));
+app.use(errorHeanler);
+//setver start function
+app.listen(port, () => {
+  console.log(`server started on port ${port}`);
+});
