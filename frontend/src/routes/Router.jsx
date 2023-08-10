d2009fc1bea432af500ec772d6925058ef533c94
yatsilmatt <yatsilmatt@gmail.com>
2023-08-10 18:03:30 +0300

    font singup done

--- frontend/src/routes/Router.jsx
+++ frontend/src/routes/Router.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import Home from "../home/Home"
 import Contact from "../home/Contact"
-import Login from "../home/Login"
 import Services from "../home/Services"
 import Singup from "../home/Singup"
 import Teacher from '../home/Teacher'
+import Connect from '../components/Connect'
 import {Routes , Route} from 'react-router-dom'
 
 
@@ -15,10 +15,10 @@ const Router = () => {
       <Route path='' element={<Home/>} />
       <Route path='/home' element={<Home/>} />
       <Route path='/contact' element={<Contact/>} />
-      <Route path='/login' element={<Login/>} />
       <Route path='/services' element={<Services/>} /> 
       <Route path='/singup' element={<Singup/>} />
       <Route path='/teacher' element={<Teacher/>} />
+      <Route path='/connect' element={<Connect/>} />
     </Routes>
    </>
   )
